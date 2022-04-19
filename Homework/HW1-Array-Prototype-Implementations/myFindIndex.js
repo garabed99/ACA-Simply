523214763c99a4c9aaa922d80c2209f6f8e993a4
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2022-04-19 21:26:27 +0400

    myFindIndex comitted

---
+++ Homework/HW1-Array-Prototype-Implementations/myFindIndex.js
@@ -0,0 +1,11 @@
+Array.prototype.myFindIndex = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+const array1 = [5, 12, 8, 130, 44];
+console.log(array1.myFindIndex((element) => element > 13)); //expected 3
