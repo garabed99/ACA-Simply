ac22f37ad5b22177a9998ec0720e70008f349ebb
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2022-04-26 19:30:27 +0400

    feat(Homework): HW2 folder setup

---
+++ Homework/HW2/HW2-Kingdom/kingdomRoadSolver.js
@@ -0,0 +1,29 @@
+function solution(roadRegister) {
+  // Implementation
+}
+
+// the output should be true
+const roadRegister1 = [
+  [false, true, false, false],
+  [false, false, true, false],
+  [true, false, false, true],
+  [false, false, true, false],
+];
+
+// the output should be true
+const roadRegister2 = [
+  [false, true, false, false, false, false, false],
+  [true, false, false, false, false, false, false],
+  [false, false, false, true, false, false, false],
+  [false, false, true, false, false, false, false],
+  [false, false, false, false, false, false, true],
+  [false, false, false, false, true, false, false],
+  [false, false, false, false, false, true, false],
+];
+
+// the output should be false
+const roadRegister = [
+  [false, true, false],
+  [false, false, false],
+  [true, false, false],
+];
