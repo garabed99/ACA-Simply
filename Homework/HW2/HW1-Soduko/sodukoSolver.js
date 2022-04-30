d361c973cf8fcf895d062f924cb989a5bee2b9fe
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2022-04-30 23:03:49 +0400

    sodukoSolver committed with incorrect subgrid check

--- Homework/HW2/HW1-Soduko/sodukoSolver.js
+++ Homework/HW2/HW1-Soduko/sodukoSolver.js
@@ -1,5 +1,37 @@
 function solution(grid) {
-  // Implementation
+  let tempRow = [];
+  let tempCol = [];
+  // let tempSub = [];
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let subGridRow = i - (i % 3);
+      let subGridCol = j - (j % 3);
+      let subGridValue = true;
+      //check subgrid
+      for (let x = subGridRow; x < subGridRow + 3; x++) {
+        for (let y = subGridCol; y < subGridCol + 3; y++) {
+          if (grid[i][j] == "." || grid[i][j] === grid[x][y]) {
+            subGridValue = false;
+          }
+        }
+      }
+      //check overall grid row/column
+      if (
+        tempRow.includes(grid[i][j]) ||
+        tempCol.includes(grid[j][i]) ||
+        subGridValue
+        // || tempSub.includes[subGridRow][subGridCol]
+      )
+        return false;
+      if (grid[i][j] != ".") tempRow.push(grid[i][j]);
+      if (grid[j][i] != ".") tempCol.push(grid[j][i]);
+    }
+    tempRow = [];
+    tempCol = [];
+    // tempSub = [];
+  }
+  return true;
 }
 
 // the output should be true
