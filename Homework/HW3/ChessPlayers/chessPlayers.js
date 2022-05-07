f9787c415c721ffe87a54b9a53a919ff0ab0a430
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2022-05-07 23:46:17 +0400

    chessPlayer committed

--- Homework/HW3/ChessPlayers/chessPlayers.js
+++ Homework/HW3/ChessPlayers/chessPlayers.js
@@ -1,3 +1,41 @@
+/*
+4 players 0 1 2 3
+
+game:                     played:               to-be-played:
+0v1   0v2   0v3           0v1   0v2             0v3
+1v2   1v3                 1v2                   1v3
+2v3                                             2v3
+
+*/
+
 const solution = (chessPlayers, finishedMatches) => {
-  // your code
+  let result = [];
+  let includes1;
+  let includes2;
+
+  for (let i = 0; i < chessPlayers; i++) {
+    for (let j = i + 1; j < chessPlayers; j++) {
+      includes1 = finishedMatches.some((a) =>
+        [i, j].every((v, z) => v === a[z])
+      );
+
+      includes2 = finishedMatches.some((a) =>
+        [j, i].every((v, z) => v === a[z])
+      );
+
+      if (!includes1 && !includes2) {
+        result.push([i, j]);
+      }
+    }
+  }
+  return result;
 };
+
+let chessPlayers = 4;
+let finishedMatches = [
+  [0, 1],
+  [1, 2],
+  [2, 0],
+];
+
+console.log(solution(chessPlayers, finishedMatches));
